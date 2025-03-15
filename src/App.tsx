f3d23ec7b747593225b25565e1418851f94cb6dc
Loc Nguyen <lnguyen72@huskers.unl.edu>
2025-03-15 15:47:41 -0500

    Prelim hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Hero from "./components/Hero";
+import "./index.css"
 
 function App() {
   return (
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,14 +2,14 @@ import { FC } from "react";
 
 const Hero: FC = () => {
     return (
-        <div>
-            <h1>
-                Hello, I'm
+        <div className="flex flex-col gap-4 flex-grow basis-0">
+            <h1 className="text-6xl md:text-5xl lg:text-6xl xl:text-8xl font-[Sigmar] text-white">
+                Hello
             </h1>
-            <h2>
-                Loc Nguyen
+            <h2 className="text-3xl md:text-2xl lg:text-4xl xl:text-5xl  text-white font-bold">
+                I'm <b><span style={{color: "rgb(255, 219, 187)"}}>Loc Nguyen</span></b>
             </h2>
-            <h3>
+            <h3 className="text-2xl md:text-xl lg:text-2xl xl:text-2xl  text-white">
                 Computer Science and Mathematics Student
             </h3>
         </div>
