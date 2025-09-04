06b10d85499e2205dea558d2a4eaeeeb934b3839
Loc Nguyen <lnguyen72@huskers.unl.edu>
2025-09-04 11:53:06 -0500

    add some warning messages

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -7,6 +7,9 @@ const Experience: React.FC = () => {
       <div className="max-w-2xl w-full space-y-4">
         <Link to="/" className="text-green-400 hover:text-green-300 transition-colors">&larr; back</Link>
         <h1 className="text-4xl font-bold text-white">hi, this is an experience page.</h1>
+        <p className="text-white">
+          oops, you found this page. there's nothing here yet, but feel free to come back later!
+        </p>
       </div>
     </div>
   );
