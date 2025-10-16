919bf48b1a5596ebbb909dc05540ad0483472e51
Loc Nguyen <lnguyen72@huskers.unl.edu>
2025-10-15 22:37:13 -0500

    simple about me

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,10 +7,9 @@ const About: React.FC = () => {
     <div className="min-h-screen flex items-center justify-center p-8">
       <div className="max-w-2xl w-full space-y-4">
         <Link to="/" className="text-green-400 hover:text-green-300 transition-colors">&larr; back</Link>
-        <h1 className="text-4xl font-bold text-white">hi, this is an about page.</h1>
+        <h1 className="text-4xl font-bold text-white">about me</h1>
         <p className="text-white">
-          oops, you found this page. there's nothing here yet, but feel free to come back later!
-          <img src={Lloid}/>
+          i'm a fourth year computer science and mathematics student at the university of nebraska-lincoln with declared focus areas in foundations of computer science and software engineering. i have previous experience working on full-stack web and mobile applications &#58;&#41;
         </p>
       </div>
     </div>
